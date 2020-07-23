1f595e4a8d3b76197cb8bdbb0ffd738ef0df167f
Shiwei Pan <l>
2020-07-23 12:54:31 +0800

    file

--- fourier.m
+++ fourier.m
@@ -1,25 +1,22 @@
+clc;clear
 subplot(2,2,1);title('原始');
 hold on
 t = 0:0.05:10;                     
 u=1;
-r=1;
-e=0;%用于交换变量
 for i=1:10
-    for j=u:u+20
-    y(j)=r;
+    for j=1:20
+        y(u+j)=2*rand-1;
     end
     u=u+20;
-    q=e;
-    e=r;
-    r=q;
 end
+disp(y)
 plot(t,y)
 subplot(2,2,2);title('振幅');
 hold on
-a = fft(y); 
+a = fft(y);
+disp(a)
 for i=1:201
     b(i)=real(a(i));
-    b(i)=abs(b(i));
 end
 plot(t,b)
 subplot(2,2,3);title('相位');
@@ -33,13 +30,11 @@ subplot(2,2,4);title('逆傅里叶变换(使用低通滤波)');
 ylim=([0,1.5]);
 hold on
 for i=1:201
-    disp(real(a(i)))
     if(abs(real(a(i)))>1)
-        f(i)=0;
+        f(i)=0;%去掉高频
     else
         f(i)=abs(real(a(i)));
     end
 end
-disp(f)
 d=ifft(f);
 plot(t,d)
